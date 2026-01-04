04f3c24f3c3e96bb636c88e527502b88ccbb24c1
Piyush Tewari <tewaripiyush43@gmail.com>
2026-01-05 02:23:34 +0530

    chore: initial project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rate Limiting Service
+
+A backend project for implementing rate limiting using Node.js, TypeScript, Express, and Redis.
+
+> Work in progress.
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res)=>{
+  res.send({status: "ok"})
+})
+
+export default app;
